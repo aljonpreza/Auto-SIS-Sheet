766cbeb06b059076cdefc703d355dfe71d6be0c0
Aljon Preza <aljonpreza@yahoo.com>
2016-11-08 15:10:12 -1000

    Completed Lexus Auto SIS
    ARGS: <xlsxName> <sheetName> <fileName>

--- autosis.py
+++ autosis.py
@@ -6,12 +6,41 @@ import requests
 import json
 import argparse
 
+def parseOptions():
+    # Command Line options
+    if len(sys.argv) != 4:
+        print 'usage: ./webscraperLexus <xlsxName> <sheetName> <fileName>'
+        sys.exit(0)
+
+    ret = {
+        "xlsxName": sys.argv[1],
+        "sheetName": sys.argv[2],
+        "fileName": sys.argv[3]
+    }
+
+    return ret
+
 def main():
-    wb = load_workbook(filename='SISSheet.xlsx', data_only = True)
+    # Parses the options from the user
+    opts = parseOptions()
+    print opts
+
+    try:
+        wb = load_workbook(filename = opts["xlsxName"] + '.xlsx', data_only = True)
+    except IOError:
+        print "No xlsx file with that name."
+        sys.exit(0)
 
     # Array of all sheets in spreadsheet
     wbSheets = wb.get_sheet_names()
-    ws = wb[wbSheets[2]]
+
+    # Check whether sheet name given is valid
+    if opts["sheetName"] in wbSheets:
+        sheetIndex = wbSheets.index(opts["sheetName"])
+        ws = wb[wbSheets[sheetIndex]]
+    else:
+        print "Name of sheet not found in xlsx."
+        sys.exit(0)
 
     vehicleTypeYear = (ws['A1'].value).split()
     build = OrderedDict([
@@ -22,45 +51,67 @@ def main():
         ("packages", [])
     ])
 
-    flag = 1
+    flag = 1 # Denotes end of available package iteration
     packageName = ""
     packagePrice = ""
     packageDetails = ""
-
+    availColorRowStart = None # Used to mark the start of color selections
     # Iteration of Available Packages
     for row in ws.rows:
         for cell in row:
             if 'CHOICE' in str(cell.value):
-                packageName = cell.value
+                packageName = cell.value[len('CHOICE \"'):-len("\"")]
             if cell.column == 'F' and cell.value != None:
                 if row[4].value == None:
                     packageDetails += row[6].value + "\n"
                 else:
                     packageDetails += "%s ($%d)\n" % (row[6].value, row[9].value)
             if cell.column == 'I' and cell.value != None:
-                packageNameCheck = packageName.lower()
-                cellValueLow = cell.value.lower()
-                if cellValueLow.startswith(packageNameCheck) and row[9].value:
+                # Gets total price of package, this also notes the end of a package.
+                cellValue = cell.value[len('CHOICE \"'):-len("\" Total:")]
+                if packageName == cellValue and row[9].value:
                     packagePrice = row[9].value
                     package = {
                         "packageName": packageName,
                         "packagePrice": packagePrice,
-                        "packageDetails": packageDetails
+                        "packageDetails": packageDetails,
+                        "exColors": [],
+                        "intColors": []
                     }
                     build["packages"].append(package)
                     packageDetails = ""
 
-            # End of Available Packages, Prints Color Combo Starting Row
+            # End of all available packages, prints color combo starting row
             if cell.value == 'AVAILABLE COLOR COMBINATIONS':
-                print "Color Combinations starting at: (%s%s)" % (cell.column, cell.row)
+                availColorRowStart = cell.row
                 flag = 0
         if flag == 0:
             break
 
-    print build
+    # Get Colors
+    exColors = []
+    intColors = []
+    numChoice = -1
 
-    with open("LexusGS.json", 'w') as outfile:
-         json.dump(build, outfile, indent = 2)
+    # Get interior and exterior colors of packages
+    for row in ws[availColorRowStart:ws.max_row]:
+        for cell in row:
+            if cell.column == 'B' and cell.value != None and cell.value != 'CHOICE(S)':
+                numChoice += 1
+                currentChoice = cell.value
+            # Inserts color to exterior and interior selection array without duplicates
+            if cell.column == 'G' and cell.value != None and cell.value != 'COLOR':
+                tempExt = {"colorName": cell.value, "isApplicable": True}
+                if tempExt not in build['packages'][numChoice]['exColors']:
+                    build['packages'][numChoice]['exColors'].append(tempExt)
+            if cell.column == 'J' and cell.value != None and cell.value != 'COLOR':
+                tempInt = {"colorName": cell.value, "isApplicable": True}
+                if tempInt not in build['packages'][numChoice]['intColors']:
+                    build['packages'][numChoice]['intColors'].append(tempInt)
+
+    with open("./scripts/json/" + opts["fileName"] + ".json", 'w') as outfile:
+        json.dump(build, outfile, indent = 2)
+    print "\nJSON scraped into " + opts["fileName"] + ".json"
 
 if __name__ == '__main__':
     main()
